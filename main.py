83976429a68a246385c7ce90f9801eb05d6ca356
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-13 18:59:29 +0300

    22. Integrating Real-World Search with Tavily and LangChain Tools

--- main.py
+++ main.py
@@ -6,27 +6,17 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
 
-@tool
-def search(query: str) -> str:
-    """
-    Инструмент поиска в интернет
-    Args:
-        query: Запрос для поиска
-    Returns:
-        Результат поиска
-    """
-    print(f"Поиск по {query}")
-    return "Погода в Токио солнечная"
 
 llm = ChatOpenAI(model="gpt-5")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="Какая погода в Токио?")})
+    result = agent.invoke({"messages":HumanMessage(content="Поиск 3 вакансий java разработчика на hh.uz в Узбекистане и отображение их деталей")})
     print(result)
 
 
