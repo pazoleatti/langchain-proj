907cee5f2eda8a18955b9e01f980ec724ce15959
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-13 19:51:30 +0300

    23. Structured Output with LangChain Agents Using Pydantic

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
+from typing import List
 
+class Source(BaseModel):
+    """Схема для источника используемого агентом"""
+    url:str = Field(description="URL источника")
+
+class AgentResponse(BaseModel):
+    """Схема для ответа агента"""
+    answer:str = Field(description="Ответ агента на запрос и источник")
+    sources:List[Source] = Field(default_factory=list, description="Список источников используемых для генерации ответа")
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
