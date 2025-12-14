695208f8b7f97c015f98a970dbe2e01ab2ed92e3
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-14 21:13:22 +0300

    27. From prompt to execution

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
+from itertools import chain
+
 from dotenv import load_dotenv
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
 load_dotenv()
 
 tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = PromptTemplate.from_template(
+"""Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}"""
+)
+
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt
+)
+
+agent_executor = AgentExecutor(agent = agent, tools=tools, verbose=True)
+chain = agent_executor
 
 def main():
-    print("Hello from langchain-course!")
+    result = chain.invoke(
+        input={
+            "input": "Поиск 3 вакансий java разработчика на hh.uz в Узбекистане и отображение их деталей"
+        }
+    )
 
 
 if __name__ == "__main__":
