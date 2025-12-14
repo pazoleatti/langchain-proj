88fef6b3031724d79aa29a3ff761ee4b929337ba
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-14 20:27:45 +0300

    25. Env and dependency setup for a LangChain Agent

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
