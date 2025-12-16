9546b3210cd6585fb0cc36560007f19cf5818a85
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-16 20:41:00 +0300

    29. Enhancing LangChain Agents with Pydantic Output Parsers

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from dotenv import load_dotenv
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
@@ -33,11 +38,16 @@ Begin!
 Question: {input}
 Thought:{agent_scratchpad}"""
 )
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"]
+).partial(format_instructions=output_parser.get_format_instructions())
 
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt
+    prompt=react_prompt_with_format_instructions
 )
 
 agent_executor = AgentExecutor(agent = agent, tools=tools, verbose=True)
@@ -46,9 +56,10 @@ chain = agent_executor
 def main():
     result = chain.invoke(
         input={
-            "input": "Поиск 3 вакансий java разработчика на hh.uz в Узбекистане и отображение их деталей"
+            "input": "Поиск 3 вакансий java разработчика на linkedin в Узбекистане и отображение их деталей"
         }
     )
+    print(result)
 
 
 if __name__ == "__main__":
