f7d9226a5fa2b2de3332ac3275982582f3bfa94e
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-16 21:09:07 +0300

    30. Composing an Agent with Output Parsers using LCEL

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from itertools import chain
-
 from dotenv import load_dotenv
-from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
 from langchain_core.output_parsers.pydantic import PydanticOutputParser
@@ -16,28 +13,8 @@ from schemas import AgentResponse
 load_dotenv()
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4")
-react_prompt = PromptTemplate.from_template(
-"""Answer the following questions as best you can. You have access to the following tools:
+    llm = ChatOpenAI(model="gpt-4")
 
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}"""
-)
 output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
@@ -51,7 +28,9 @@ agent = create_react_agent(
 )
 
 agent_executor = AgentExecutor(agent = agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda  x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
 
 def main():
     result = chain.invoke(
